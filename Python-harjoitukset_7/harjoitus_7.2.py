ac619654d5fc36c7c581c1b13cd1ab2ceac7ef84
reimakoo <reimaku@metropolia.fi>
2022-09-14 13:45:12 +0300

    Vähä kotiläksyjä vähä

--- Python-harjoitukset_7/harjoitus_7.2.py
+++ Python-harjoitukset_7/harjoitus_7.2.py
@@ -3,22 +3,20 @@ nimilista = set()
 
 print("Syötä minulle nimiä! Kun haluat lopettaa, paina vain enteriä!")
 nimi = input("Syötä nimi!: ")
-print("Uusi nimi")
+
 
 while nimi != "":
-    nimilista.add(nimi)
-    nimi = input("Syötä nimi!: ")
 
     if nimi in nimilista:
         nimilista.add(nimi)
         print("Aiemmin syötetty nimi")
-        nimi = input("Syötä nimi!: ")
+
 
     else:
         nimilista.add(nimi)
         print("Uusi nimi")
-        nimi = input("Syötä nimi!: ")
-
+    nimilista.add(nimi)
+    nimi = input("Syötä nimi!: ")
 
 while nimi == "":
     for nimi in nimilista:
